3c2ae28957da620549aa18a5ba7588b141b9fa0b
yujale <130224287+yujale@users.noreply.github.com>
2023-06-07 19:10:37 +0800

    feat:python 数据类型

---
+++ python-basics/05-数据类型.py
@@ -0,0 +1,42 @@
+if __name__ == "__main__":
+    # 数据类型查看的函数 type(数据/变量名)
+    # 基础数据类型:int  float  bool
+    # 容器类型: str  list tuple  set dict
+
+    # 整型
+    int1 = 12
+    print(type(int1))  # <class 'int'>
+    # 浮点型
+    float1 = 12.1
+    print(type(float1))  # <class 'float'>
+    # 布尔型  (True/False)
+    bool1 = True
+    print(type(bool1))  # <class 'bool'>
+    # 字符串型
+    str1 = 'hello Python'
+    print(type(str1))  # <class 'str'>
+    # 元组
+    tuple1 = (1, 2, 3, 4)
+    print(type(tuple1))  # <class 'tuple'>
+    # 列表
+    list1 = [1, 2, 3, 4]
+    print(type(list1))  # <class 'list'>
+    # 集合
+    set1 = {1, 2, 3, 4}
+    print(type(set1))  # <class 'set'>
+    # 字典
+    dict1 = {'name': 'xiaoming', 'age': 18}
+    print(type(dict1))  # <class 'dict'>
+
+    # 代码格式化的快捷键:ctrl + alt + L
+
+    a = 12
+    # a是什么数据类型? int
+    a = 'str'
+    # a是什么数据类型? str
+    a = '12'
+    # a是什么数据类型? str
+    a = True
+    # a是什么数据类型? bool
+    a = 13.4
+    # a是什么数据类型? float
