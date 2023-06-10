6c0ca827b26d6829926419a051fcdea9f19a185a
yujale98 <yujiale@yujialedeMacBook-Pro-2.local>
2023-06-10 23:37:41 +0800

    feat:bs4 初次小说成功

--- 爬虫/01-数据解析/02-bs4/00-bs4 爬取笔趣阁电子书.py
+++ 爬虫/01-数据解析/02-bs4/00-bs4 爬取笔趣阁电子书.py
@@ -12,30 +12,28 @@ response = requests.get(url=url, headers=headers)
 response.encoding = 'utf-8'
 result = response.text
 soup = BeautifulSoup(result,"html.parser")
-# a_list =soup.find('div',name='listmain')
 a_list = soup.select('.listmain > dl')
 fp = open('./sanguo.txt','w',encoding='utf-8')
 for a in a_list:
-    print(a)
-
-    # title = a.string
-    # detail_url = a['href']
-    # detail_url= detail_url[11:len(detail_url)]
-    # print(detail_url)
-    # if detail_url == 'javascript:dd_show()':
-    #     continue
-    # detail_url = url + detail_url
-    # # 获取章节内容
-    # response = requests.get(url=detail_url, headers=headers)
-    # response.encoding = 'utf-8'
-    # result = response.text
-    # soup = BeautifulSoup(result, features="html")
-    # div_tag = soup.find('div', id='chaptercontent')
-    # content = div_tag.text
-    # fp.write(title + '\n' + content + '\n')
-    # print(content)
-    # print(title, '爬取保存成功')
-# fp.close()
+    result_a = a.findAll('a')
+    for b in result_a:
+        title = b.string
+        detail_url = b['href']
+        detail_url= detail_url[11:len(detail_url)]
+        detail_url = url + detail_url
+        # 获取章节内容
+        response = requests.get(url=detail_url, headers=headers)
+        response.encoding = 'utf-8'
+        result = response.text
+        soup = BeautifulSoup(result, features="html")
+        div_tag = soup.find('div', id='chaptercontent')
+        if div_tag is None:
+            continue
+        content = div_tag.text
+        fp.write(title + '\n' + content + '\n')
+        print(content)
+        print(title, '爬取保存成功')
+fp.close()
 
 
 
