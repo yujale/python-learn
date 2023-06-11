5cc0764ffc0d146545e46f6ad1c16942593b7372
yujale98 <yujiale@yujialedeMacBook-Pro-2.local>
2023-06-11 20:17:49 +0800

    feat:数据解析 xpath 爬取简历模版

---
+++ 爬虫/01-数据解析/03-xpath/04-爬取简历模版.py
@@ -0,0 +1,36 @@
+from lxml import etree
+import requests
+import os
+
+headers: dict[str, str] = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/97.0.4692.71 Safari/537.36'
+}
+dirName = '简历'
+if not os.path.exists(dirName):
+    os.mkdir(dirName)
+
+url = 'https://sc.chinaz.com/jianli/free.html'
+response = requests.get(url=url, headers=headers)
+response.encoding = 'utf-8'
+result = response.text
+tree = etree.HTML(result)
+div_list = tree.xpath('//*[@id="container"]/div')
+
+for div in div_list:
+    title: object = div.xpath('./p/a/text()')[0] + '.rar'
+    detail_url = div.xpath('./p/a/@href')[0]
+    response = requests.get(url=detail_url, headers=headers)
+    response.encoding = 'utf-8'
+    detail_page_text = response.text
+    print(detail_page_text)
+    # 数据解析：下载地址
+
+    tree = etree.HTML(detail_page_text)
+    download_url = tree.xpath('//*[@id="down"]/div[2]/ul/li[1]/a/@href')[0]
+    # 在下载请求建立模板
+    data = requests.get(url=download_url, headers=headers).content
+    img_path = dirName + '/' + title
+    with open(img_path, 'wb') as fp:
+        fp.write(data)
+    print(title, "保存下载成功！")
