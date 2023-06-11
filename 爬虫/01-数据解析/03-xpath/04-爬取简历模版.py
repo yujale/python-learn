30127883d599fd52777d50789a5fa27d02de5204
yujale98 <yujiale@yujialedeMacBook-Pro-2.local>
2023-06-11 20:24:59 +0800

    feat:数据解析 xpath 爬取多页简历模版

--- 爬虫/01-数据解析/03-xpath/04-爬取简历模版.py
+++ 爬虫/01-数据解析/03-xpath/04-爬取简历模版.py
@@ -10,27 +10,31 @@ dirName = '简历'
 if not os.path.exists(dirName):
     os.mkdir(dirName)
 
-url = 'https://sc.chinaz.com/jianli/free.html'
-response = requests.get(url=url, headers=headers)
-response.encoding = 'utf-8'
-result = response.text
-tree = etree.HTML(result)
-div_list = tree.xpath('//*[@id="container"]/div')
+url = 'https://sc.chinaz.com/jianli/free_%d.html'
 
-for div in div_list:
-    title: object = div.xpath('./p/a/text()')[0] + '.rar'
-    detail_url = div.xpath('./p/a/@href')[0]
-    response = requests.get(url=detail_url, headers=headers)
+for i in range(1, 6):
+    if i == 1:
+        new_url = 'https://sc.chinaz.com/jianli/free.html'
+    else:
+        new_url = format(url % i)
+    print("----------正在请求下载第%d页的简历数据----------" % i)
+    response = requests.get(url=new_url, headers=headers)
     response.encoding = 'utf-8'
-    detail_page_text = response.text
-    print(detail_page_text)
-    # 数据解析：下载地址
-
-    tree = etree.HTML(detail_page_text)
-    download_url = tree.xpath('//*[@id="down"]/div[2]/ul/li[1]/a/@href')[0]
-    # 在下载请求建立模板
-    data = requests.get(url=download_url, headers=headers).content
-    img_path = dirName + '/' + title
-    with open(img_path, 'wb') as fp:
-        fp.write(data)
-    print(title, "保存下载成功！")
+    result = response.text
+    tree = etree.HTML(result)
+    div_list = tree.xpath('//*[@id="container"]/div')
+    for div in div_list:
+        title = div.xpath('./p/a/text()')[0] + '.rar'
+        detail_url = div.xpath('./p/a/@href')[0]
+        response = requests.get(url=detail_url, headers=headers)
+        response.encoding = 'utf-8'
+        detail_page_text = response.text
+        # 数据解析：下载地址
+        tree = etree.HTML(detail_page_text)
+        download_url = tree.xpath('//*[@id="down"]/div[2]/ul/li[1]/a/@href')[0]
+        # 在下载请求建立模板
+        data = requests.get(url=download_url, headers=headers).content
+        img_path = dirName + '/' + title
+        with open(img_path, 'wb') as fp:
+            fp.write(data)
+        print(img_path, "保存下载成功！")
