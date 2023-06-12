bb0ab6213b6274e2a1e7d600b55f9c02c5749cc4
yujale98 <yujiale@yujialedeMacBook-Pro-2.local>
2023-06-12 19:50:35 +0800

    Revert "feat:爬虫 图片防盗链爬取"
    This reverts commit b5c3d44db1c3f8658636ebda328d90a80a795f08.

--- 爬虫/02-cookie-代理-验证码-防盗链/03-防盗链/00-图片防盗链.py
+++ 爬虫/02-cookie-代理-验证码-防盗链/03-防盗链/00-图片防盗链.py
@@ -1,7 +1,6 @@
 from lxml import etree
 import requests
 
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/97.0.4692.71 Safari/537.36',
