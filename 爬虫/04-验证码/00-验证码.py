c6a42278d54ef3954849421fa952d37f383645d1
yujale98 <yujiale@yujialedeMacBook-Pro-2.local>
2023-06-12 19:38:50 +0800

    feat:爬虫使用打码获取验证码

--- 爬虫/04-验证码/00-验证码.py
+++ 爬虫/04-验证码/00-验证码.py
@@ -1,61 +1,6 @@
-import base64
-import json
-import requests
-
-
-# 一、图片文字类型(默认 3 数英混合)：
-# 1 : 纯数字
-# 1001：纯数字2
-# 2 : 纯英文
-# 1002：纯英文2
-# 3 : 数英混合
-# 1003：数英混合2
-#  4 : 闪动GIF
-# 7 : 无感学习(独家)
-# 11 : 计算题
-# 1005:  快速计算题
-# 16 : 汉字
-# 32 : 通用文字识别(证件、单据)
-# 66:  问答题
-# 49 :recaptcha图片识别
-# 二、图片旋转角度类型：
-# 29 :  旋转类型
-#
-# 三、图片坐标点选类型：
-# 19 :  1个坐标
-# 20 :  3个坐标
-# 21 :  3 ~ 5个坐标
-# 22 :  5 ~ 8个坐标
-# 27 :  1 ~ 4个坐标
-# 48 : 轨迹类型
-#
-# 四、缺口识别
-# 18 : 缺口识别（需要2张图 一张目标图一张缺口图）
-# 33 : 单缺口识别（返回X轴坐标 只需要1张图）
-# 五、拼图识别
-# 53：拼图识别
-# 函数实现忽略
-def base64_api(uname, pwd, img, typeid):
-    with open(img, 'rb') as f:
-        base64_data = base64.b64encode(f.read())
-        b64 = base64_data.decode()
-    data = {"username": uname, "password": pwd, "typeid": typeid, "image": b64}
-    result = json.loads(requests.post("http://api.ttshitu.com/predict", json=data).text)
-    if result['success']:
-        return result["data"]["result"]
-    else:
-        return result["message"]
-    return ""
-
-
-def getImgCodeText(imgPath, imgType):  # 直接返回验证码内容
-    # imgPath：验证码图片地址
-    # imgType：验证码图片类型
-    result = base64_api(uname='bb328410948', pwd='bb328410948', img=imgPath, typeid=imgType)
-    return result
-
-
 from lxml import etree
+import requests
+import verificationCode
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -71,5 +16,5 @@ with open('./code.jpg', 'wb') as fp:
     fp.write(code_data)
 
 # 识别验证码图片内容
-result = getImgCodeText('./code.jpg', 3)
+result = verificationCode.getImgCodeText('./code.jpg', 3)
 print(result)
